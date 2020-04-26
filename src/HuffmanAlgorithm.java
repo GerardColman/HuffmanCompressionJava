98332541d4531967ecb88582b5e532c6e1b2c69b
Gerard Colman <gerard.colman1@gmail.com>
2020-04-26 15:15:35 +0100

    Added createTree Function

--- src/HuffmanAlgorithm.java
+++ src/HuffmanAlgorithm.java
@@ -1,6 +1,5 @@
 import java.io.File;
-import java.util.Map;
-import java.util.SortedMap;
+import java.util.*;
 
 /**
  * @author Gerard Colman - 18327576
@@ -19,6 +18,10 @@ public class HuffmanAlgorithm {
             this.leftChild = leftChild;
             this.rightChild = rightChild;
         }
+        public Node(){
+            leftChild = null;
+            rightChild = null;
+        }
         public boolean isLeaf(){
             return leftChild==null && rightChild==null;
         }
@@ -27,16 +30,38 @@ public class HuffmanAlgorithm {
         }
     }
     Node root;
-    SortedMap frequencyMap;
+    private SortedMap<Integer,Character> frequencyMap = new TreeMap<Integer, Character>();
     public void encode(int[] priorityArray){
     }
 
     /** Creates a huffman binary tree based on frequencyMap
      * @author Gerard Colman - 18327576
+     * @return root Returns root of the huffman encoding tree
      */
-    public void createTree(){
-        while(!frequencyMap.isEmpty()){
-
+    public Node createTree(){
+        SortedMap<Integer, Node> PQ = new TreeMap<Integer, Node>();
+        /* REMINDERS
+        * Gerard when frequency map is made remove casts
+         */
+        while(!frequencyMap.isEmpty()){ //Putting characters from frequencyMap into a node and storing those nodes in
+            Integer key = frequencyMap.firstKey(); //Key = frequency, firstKey gets lowest key
+            Character data = frequencyMap.get(key);
+            Node node = new Node(data, key, null,null);
+            PQ.put(key,node);
+            frequencyMap.remove(key);
         }
+        while(PQ.size() > 1){
+            Node node1 = PQ.get(PQ.firstKey()); //Gets first lowest node
+            PQ.remove(PQ.firstKey()); //Removes it from frequency Map
+            Node node2 = PQ.get(PQ.firstKey()); //Gets second Lowest node
+            PQ.remove(PQ.firstKey());
+            Node parentNode = new Node();
+            parentNode.leftChild = node1;
+            parentNode.rightChild = node2;
+            Integer combinedFreq = node1.frequency + node2.frequency; //Gets combined frequency
+            parentNode.frequency = combinedFreq;
+            PQ.put(combinedFreq, parentNode);
+        }
+        return PQ.get(PQ.lastKey()); //Returns root of tree
     }
 }
