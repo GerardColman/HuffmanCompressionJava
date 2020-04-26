ddbffbee51c274524556a3a36e3497dcd0355b42
Gerard Colman <gerard.colman1@gmail.com>
2020-04-26 13:50:23 +0100

    Added node class & started encoding method

--- src/HuffmanAlgorithm.java
+++ src/HuffmanAlgorithm.java
@@ -1,18 +1,42 @@
 import java.io.File;
-import java.util.PriorityQueue;
+import java.util.Map;
+import java.util.SortedMap;
 
+/**
+ * @author Gerard Colman - 18327576
+ * @author Lukasz Filanowski - LUKAS FILL IN STUDENT NUMBER!
+ * COMP20290 Assignment 2, Huffman Compression
+ * This was completed as a team
+ */
 public class HuffmanAlgorithm {
-    public static void main(String[] args){
-        //Run get input from file
-        //Run encode
-        //Outfile
+    public static class Node{
+        private char data;
+        private int frequency;
+        private Node leftChild, rightChild;
+        public Node(char data, int frequency, Node leftChild, Node rightChild){
+            this.data = data;
+            this.frequency = frequency;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        }
+        public boolean isLeaf(){
+            return leftChild==null && rightChild==null;
+        }
+        public int compareTo(Node node){
+            return this.frequency - node.frequency;
+        }
     }
-    public File encode(int[] input){
-        PriorityQueue<String> huffmanPQ = new PriorityQueue<>();
-        /*
-        *
-        * CHECK DATASTRUCTURE AND ALGORTIHMS BOOK PAGE 615/597
-        *
-         */
+    Node root;
+    SortedMap frequencyMap;
+    public void encode(int[] priorityArray){
+    }
+
+    /** Creates a huffman binary tree based on frequencyMap
+     * @author Gerard Colman - 18327576
+     */
+    public void createTree(){
+        while(!frequencyMap.isEmpty()){
+
+        }
     }
 }
