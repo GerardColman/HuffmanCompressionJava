6bc5fa473a0fca4b8ebaee8bc5fc5a6e9f110225
Gerard Colman <gerard.colman1@gmail.com>
2020-05-01 15:41:51 +0100

    help

--- src/HuffmanAlgorithm.java
+++ src/HuffmanAlgorithm.java
@@ -14,29 +14,36 @@ public class HuffmanAlgorithm {
      * 2. Check this actually works
      * 3. Do question 3 - Lukas
      */
-    public static class Node{
+    public static class Node {
         private char data;
         private int frequency;
         private Node leftChild, rightChild;
-        public Node(char data, int frequency, Node leftChild, Node rightChild){
+
+        public Node(char data, int frequency, Node leftChild, Node rightChild) {
             this.data = data;
             this.frequency = frequency;
             this.leftChild = leftChild;
             this.rightChild = rightChild;
         }
-        public Node(){
+
+        public Node() {
             leftChild = null;
             rightChild = null;
         }
-        public boolean isLeaf(){
-            return leftChild==null && rightChild==null;
+
+        public boolean isLeaf() {
+            return leftChild == null && rightChild == null;
         }
-        public int compareTo(Node node){
+
+        public int compareTo(Node node) {
             return this.frequency - node.frequency;
         }
     }
-    private SortedMap<Integer,Character> frequencyMap = new TreeMap<Integer, Character>();
-    public HuffmanAlgorithm(){ }
+
+    private SortedMap<Integer, Character> frequencyMap = new TreeMap<Integer, Character>();
+
+    public HuffmanAlgorithm() {
+    }
 
     public void createFrequencyTable(File file) throws FileNotFoundException {
 
@@ -46,51 +53,42 @@ public class HuffmanAlgorithm {
         try {
             int[][] frequencyArray = new int[27][1];
 
-            while(in.hasNext())
-            {
+            while (in.hasNext()) {
                 inputWord = in.next();
-                inputWord = inputWord.toLowerCase();
 
                 char[] inputArray = inputWord.toCharArray();
-                char[] spaceArray = in.next().toCharArray();
                 // \\s+
-                for(int i = 0; i < inputArray.length; i++){
+                for (int i = 0; i < inputArray.length; i++) {
                     int index = 0;
-                    if(inputArray[i] == 32){ //If input array contains a space
+                    if (inputArray[i] == 32) { //If input array contains a space
                         index = 26;
-                    }else if(inputArray[i] >= 33 && inputArray[i] <= 64){ //Ignore punctuation symbols and numbers
+                    } else if (inputArray[i] >= 33 && inputArray[i] <= 64) { //Ignore punctuation symbols and numbers
                         i++;
-                    }else if(inputArray[i] >= 65 && inputArray[i] <= 90){ //Handling capital letters
+                    } else if (inputArray[i] >= 65 && inputArray[i] <= 90) { //Handling capital letters
                         index = (inputArray[i] - 65);
-                    }else if(inputArray[i] >= 123){ //Ignore misc symbols
+                    } else if (inputArray[i] >= 123) { //Ignore misc symbols
                         i++;
-                    }else{ //Handling lowercase letters
+                    } else { //Handling lowercase letters
                         index = (inputArray[i] - 97);
                     }
-
                     frequencyArray[index][0] += 1;
                 }
-
-                frequencyArray[26][0] += spaceArray.length;
             }
 
             in.close();
 
-            for(int j = 0; j < frequencyArray.length; j++)
-            {
+            for (int j = 0; j < frequencyArray.length; j++) {
                 char character = (char) frequencyArray[j][0];
 
-                if(j == 26)
-                {
+                if (j == 26) {
                     character = ' ';
                 }
 
-                int frequency = frequencyArray[j][1];
+                int frequency = frequencyArray[j][0];
 
                 frequencyMap.put(frequency, character);
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     } //DONE
@@ -100,13 +98,11 @@ public class HuffmanAlgorithm {
         Scanner in = new Scanner(file);
 
         try {
-            while(in.hasNext()){
+            while (in.hasNext()) {
 
                 output += in.nextLine();
             }
-        }
-
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -120,45 +116,49 @@ public class HuffmanAlgorithm {
         huffman.encode(inputFileName);
     }
 
-    public void encode(String inputFileName){ //Takes in name of output file
-        try{
+    public void encode(String inputFileName) { //Takes in name of output file
+        try {
             File inputFile = new File(inputFileName);
             Scanner scanner = new Scanner(inputFile);
             /*Getting File input*/
             StringBuilder input = new StringBuilder();
-            while(scanner.hasNext()){ //Gets entire file as string
+            while (scanner.hasNext()) { //Gets entire file as string
                 input.append(scanner.next());
             }
             char[] in = input.toString().toCharArray();
             /*Getting Frequency Table*/
             createFrequencyTable(inputFile);
+            System.out.println(frequencyMap.toString());
             /*Creating Encoding Tree*/
             Node root = createTree();
+            printTree(root);
             /*Creating code word table*/
             String[] codewordTable = new String[256];
-            buildCodeTable(codewordTable,root,"");
+            buildCodeTable(codewordTable, root, "");
             /*Writing tree to file*/
             writeTree(root);
             /*Encoding*/
 
-            for(int i =0;i<input.length();i++){
+            for (int i = 0; i < input.length(); i++) {
+                //System.out.println(Arrays.toString(codewordTable));
                 String code = codewordTable[in[i]]; //Gets code for letter
-                for(int j = 0;j<code.length();j++){
-                    if(code.charAt(j) == '0'){
+                for (int j = 0; j < code.length(); j++) {
+                    if (code.charAt(j) == '0') {
                         BinaryStdOut.write(false);
                     }
-                    if(code.charAt(j) == '1'){
+                    if (code.charAt(j) == '1') {
                         BinaryStdOut.write(true);
                     }
                 }
             }
             scanner.close();
             BinaryStdOut.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     } //DONE
-    public void decompress(String inputFileName){
+
+    public void decompress(String inputFileName) {
         try {
             File inputFile = new File(inputFileName);
             Scanner scanner = new Scanner(inputFile);
@@ -179,33 +179,36 @@ public class HuffmanAlgorithm {
             }
             BinaryStdOut.close();
             scanner.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     } //DONE
-    private Node readTreeFromFile(Scanner scanner){
+
+    private Node readTreeFromFile(Scanner scanner) {
         boolean leafMeAlone = scanner.nextBoolean();
-        if(leafMeAlone){
-            return new Node(scanner.next().charAt(0),-1,null,null);
-        }else{
-            return new Node('\0',-1,readTreeFromFile(scanner),readTreeFromFile(scanner));
+        if (leafMeAlone) {
+            return new Node(scanner.next().charAt(0), -1, null, null);
+        } else {
+            return new Node('\0', -1, readTreeFromFile(scanner), readTreeFromFile(scanner));
         }
     } //DONE
 
-    /** Creates a huffman binary tree based on frequencyMap
-     * @author Gerard Colman - 18327576
+    /**
+     * Creates a huffman binary tree based on frequencyMap
+     *
      * @return root Returns root of the huffman encoding tree+
+     * @author Gerard Colman - 18327576
      */
-    public Node createTree(){
+    public Node createTree() {
         SortedMap<Integer, Node> PQ = new TreeMap<Integer, Node>(); //Sorted map to store nodes
-        while(!frequencyMap.isEmpty()){ //Putting characters from frequencyMap into a node and storing those nodes in another sorted map
+        while (!frequencyMap.isEmpty()) { //Putting characters from frequencyMap into a node and storing those nodes in another sorted map
             Integer key = frequencyMap.firstKey(); //Key = frequency, firstKey gets lowest key
             Character data = frequencyMap.get(key);
-            Node node = new Node(data, key, null,null);
-            PQ.put(key,node);
+            Node node = new Node(data, key, null, null);
+            PQ.put(key, node);
             frequencyMap.remove(key);
         }
-        while(PQ.size() > 1){
+        while (PQ.size() > 1) {
             Node node1 = PQ.get(PQ.firstKey()); //Gets first lowest node
             PQ.remove(PQ.firstKey()); //Removes it from frequency Map
             Node node2 = PQ.get(PQ.firstKey()); //Gets second Lowest node
@@ -218,24 +221,35 @@ public class HuffmanAlgorithm {
             PQ.put(combinedFreq, parentNode);
         }
         return PQ.get(PQ.lastKey()); //Returns root of tree
-    } //DONE
-    private void buildCodeTable(String[] st, Node node, String n){
-        if(!node.isLeaf()){
-            buildCodeTable(st,node.leftChild, n+'0');
-            buildCodeTable(st,node.rightChild, n+'1');
-        }else{
+    } //ERROR
+
+    private void buildCodeTable(String[] st, Node node, String n) {
+        if (!node.isLeaf()) { //Base Case
+            buildCodeTable(st, node.leftChild, n + "0"); //Recursive Call
+            buildCodeTable(st, node.rightChild, n + "1"); //Recursive Call
+        } else {
             st[node.data] = n;
         }
-    } //DONE
+    } //ERROR
+
     private void writeTree(Node root) {
         //Find alternate method of writing bits
-        if(root.isLeaf()){ //Base Case
+        if (root.isLeaf()) { //Base Case
             BinaryStdOut.write(true);
-            BinaryStdOut.write(root.data,8);
+            BinaryStdOut.write(root.data, 8);
             return;
         }
         BinaryStdOut.write(false);
         writeTree(root.leftChild);
         writeTree(root.rightChild);
     } //DONE
+
+    private void printTree(Node root) {
+        if (root.isLeaf()) {
+            return;
+        }
+        System.out.println(root.data);
+        printTree(root.leftChild);
+        printTree(root.rightChild);
+    } //TEST FUNCTION
 }
